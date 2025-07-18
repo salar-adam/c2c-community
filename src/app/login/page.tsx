977e982e9dbed7100516e566f38f3ba1ac0cea27
Salar Adam <tg219salaradam@gmail.com>
2025-07-18 07:59:56 +0000

    so remove signup for now and instead of email say name remove forget pas

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,6 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { GeoNexusLogo } from "@/components/icons"
-import Link from "next/link"
 
 export default function LoginPage() {
   return (
@@ -21,44 +20,27 @@ export default function LoginPage() {
             </div>
           <CardTitle className="text-2xl">Welcome Back</CardTitle>
           <CardDescription>
-            Enter your email below to login to your GeoNexus account
+            Enter your name and password to login
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
             <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
+              <Label htmlFor="name">Name</Label>
               <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
+                id="name"
+                type="text"
+                placeholder="Brodie"
                 required
               />
             </div>
             <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="#"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
+              <Label htmlFor="password">Password</Label>
               <Input id="password" type="password" required />
             </div>
             <Button type="submit" className="w-full">
               Login
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-              Sign up
-            </Link>
           </div>
         </CardContent>
       </Card>
