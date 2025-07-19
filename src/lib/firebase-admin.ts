9e3e36dcb768c43fe698120eee91d1493605389c
Salar Adam <tg219salaradam@gmail.com>
2025-07-19 04:37:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,5 +1,6 @@
 import "server-only"
 import * as admin from 'firebase-admin';
+import serviceAccount from "../../firebase-service-account-key.json";
 
 let firebaseAdminApp: admin.app.App;
 
@@ -12,23 +13,6 @@ function getFirebaseAdminApp() {
         firebaseAdminApp = admin.app();
         return firebaseAdminApp;
     }
-
-    if (!process.env.FIREBASE_PRIVATE_KEY) {
-        throw new Error('The FIREBASE_PRIVATE_KEY environment variable is not set. Please check your environment configuration.');
-    }
-
-    const serviceAccount = {
-        type: process.env.FIREBASE_TYPE,
-        project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: process.env.FIREBASE_AUTH_URI,
-        token_uri: process.env.FIREBASE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    };
     
     firebaseAdminApp = admin.initializeApp({
         credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
