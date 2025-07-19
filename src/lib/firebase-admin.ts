1e2b9f028c01eabb65aee88a841e6a5283df50b5
Salar Adam <tg219salaradam@gmail.com>
2025-07-19 03:59:03 +0000

    ok

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,6 +3,7 @@ import "server-only"
 import * as admin from "firebase-admin";
 import serviceAccount from "../../firebase-service-account-key.json";
 
+// Prevents re-initialization during hot-reloads in development
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
