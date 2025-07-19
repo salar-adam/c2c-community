2946f1735f88b45b2ca8717eca741059c6b50353
Salar Adam <tg219salaradam@gmail.com>
2025-07-19 05:03:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,19 +1,10 @@
 import "server-only"
 import * as admin from 'firebase-admin';
+import serviceAccount from './firebase-service-account.json';
 
 if (!admin.apps.length) {
-    const serviceAccount = {
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
-    };
-
-    if (!serviceAccount.projectId || !serviceAccount.clientEmail || !serviceAccount.privateKey) {
-        throw new Error('Firebase admin credentials are not set in .env.local');
-    }
-
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
     });
 }
 
